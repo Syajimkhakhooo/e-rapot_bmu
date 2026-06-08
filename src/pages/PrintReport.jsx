da402e4e9131d65ba0810e3ece6b59e98071c325
Admin BMU <fasyagilar80@gmail.com>
2026-06-09 00:11:37 +0700

    Remove red underline from score texts on Print Report

--- src/pages/PrintReport.jsx
+++ src/pages/PrintReport.jsx
@@ -183,7 +183,7 @@ export default function PrintReport() {
                   <div className="flex flex-col">
                     <div className="font-bold py-1 border-b-2 border-black bg-slate-50">KOSAKATA<br/>語彙</div>
                     <div className="py-2 text-lg font-bold flex items-center justify-center">
-                      <span className={Number(report.score_kosakata) < 85 ? 'text-red-600 underline decoration-2 underline-offset-4' : ''}>
+                      <span className={Number(report.score_kosakata) < 85 ? 'text-red-600' : ''}>
                         {report.score_kosakata}/100
                       </span>
                     </div>
@@ -191,7 +191,7 @@ export default function PrintReport() {
                   <div className="flex flex-col">
                     <div className="font-bold py-1 border-b-2 border-black bg-slate-50">HIRAGANA<br/>ひらがな</div>
                     <div className="py-2 text-lg font-bold flex items-center justify-center">
-                      <span className={Number(report.score_hiragana) < 85 ? 'text-red-600 underline decoration-2 underline-offset-4' : ''}>
+                      <span className={Number(report.score_hiragana) < 85 ? 'text-red-600' : ''}>
                         {report.score_hiragana}/100
                       </span>
                     </div>
@@ -199,7 +199,7 @@ export default function PrintReport() {
                   <div className="flex flex-col">
                     <div className="font-bold py-1 border-b-2 border-black bg-slate-50">KATAKANA<br/>カタカナ</div>
                     <div className="py-2 text-lg font-bold flex items-center justify-center">
-                      <span className={Number(report.score_katakana) < 85 ? 'text-red-600 underline decoration-2 underline-offset-4' : ''}>
+                      <span className={Number(report.score_katakana) < 85 ? 'text-red-600' : ''}>
                         {report.score_katakana}/100
                       </span>
                     </div>
@@ -207,7 +207,7 @@ export default function PrintReport() {
                   <div className="flex flex-col">
                     <div className="font-bold py-1 border-b-2 border-black bg-slate-50">HASIL UJIAN<br/>試験結果</div>
                     <div className="py-2 text-lg font-bold flex items-center justify-center">
-                      <span className={Number(report.score_ujian) < 85 ? 'text-red-600 underline decoration-2 underline-offset-4' : ''}>
+                      <span className={Number(report.score_ujian) < 85 ? 'text-red-600' : ''}>
                         {report.score_ujian}/100
                       </span>
                     </div>
